a97d75b621ea1fe883db772bd66649667c9841fd
Miriam Asencio <148205684+Miritzila@users.noreply.github.com>
2024-02-28 19:30:55 +0000

    first commit

--- src/models.py
+++ src/models.py
@@ -7,23 +7,51 @@ from eralchemy2 import render_er
 
 Base = declarative_base()
 
-class Person(Base):
-    __tablename__ = 'person'
-    # Here we define columns for the table person
-    # Notice that each column is also a normal Python instance attribute.
+class User(Base):
+    __tablename__ = 'user'
+    id = Column(Integer, primary_key=True)
+    username = Column(String(250), nullable=False)
+    password = Column(String(250), nullable=False)
+    favorites = relationship('Favorite', backref='user')
+
+class Planet(Base):
+    __tablename__ = 'planet'
     id = Column(Integer, primary_key=True)
     name = Column(String(250), nullable=False)
+    climate = Column(String(250), nullable=False)
+    population = Column(String(250), nullable=False)
+    diameter = Column(String(250), nullable=False)
+    favorites = relationship('Favorite', backref='planet')
 
-class Address(Base):
-    __tablename__ = 'address'
-    # Here we define columns for the table address.
-    # Notice that each column is also a normal Python instance attribute.
+class Person(Base):
+    __tablename__ = 'person'
     id = Column(Integer, primary_key=True)
-    street_name = Column(String(250))
-    street_number = Column(String(250))
-    post_code = Column(String(250), nullable=False)
-    person_id = Column(Integer, ForeignKey('person.id'))
-    person = relationship(Person)
+    name = Column(String(250), nullable=False)
+    hair_color=Column(String(250), nullable=True)
+    gender=Column(String(250), nullable=True)
+    birth_year=Column(String(250), nullable=False)
+    favorites = relationship('Favorite', backref='person')
+
+class Starship(Base):
+    __tablename__ = 'starship'
+    id = Column(Integer, primary_key=True)
+    name = Column(String(250), nullable=False)
+    model = Column(String(250), nullable=False)
+    passengers = Column(String(250), nullable=False)
+    max_atmosphering_speed = Column(String(250), nullable=False)
+    favorites = relationship('Favorite', backref='starship')
+
+class Favorite(Base):
+    __tablename__ = 'favorite'
+    id = Column(Integer, primary_key=True)
+    user_id = Column(Integer, ForeignKey('user.id'))
+    favoriteStarshipId = Column(Integer, ForeignKey('starship.id'), nullable=True)
+    favoritePersonId = Column(Integer, ForeignKey('person.id'), nullable=True)
+    favoritePlanetId = Column(Integer, ForeignKey('planet.id'), nullable=True)
+    user = relationship('User', backref='favorites')
+    starship = relationship('Starship', backref='favorites')
+    person = relationship('Person', backref='favorites')
+    planet = relationship('Planet', backref='favorites')
 
     def to_dict(self):
         return {}
